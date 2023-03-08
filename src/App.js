1858f40d33e68cb377c963caf342642f718e52a8
Emon <alauddinemon3@gmail.com>
2023-03-09 00:15:29 +0600

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Banner from "./Pages/Banner/Banner";
 import Navbar from "./Pages/Home/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Banner />
     </div>
   );
 }
--- src/Pages/Home/Navbar.js
+++ src/Pages/Home/Navbar.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import './Navbar.css';
 import logo from '../../Assets/images/Logo/cwc-logo.png';
 
 const Navbar = () => {
    return (
 
-      <div>
-         <div>
+      <div className='main-container'>
+         <div className='navlogo'>
+            {/* Logo and Name */}
             <img src={logo} alt="" />
-            <p>Carry With care 2.0</p>
+            <p>CARRY WITH CARE 2.0</p>
          </div>
-         <div></div>
+
+         <div className='contact-ds'>
+            <p>✆ +8801883-277339</p>
+            <button className="btn-fb">
+               Connect with Facebook
+            </button>
+
+         </div>
       </div>
 
    );
