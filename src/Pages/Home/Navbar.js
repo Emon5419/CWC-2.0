61091db1e1846c2af311db62b0aacd6280c0c8e3
Emon <alauddinemon3@gmail.com>
2023-03-08 21:36:16 +0600

    first commit

---
+++ src/Pages/Home/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../Assets/images/Logo/cwc-logo.png';
+
+const Navbar = () => {
+   return (
+
+      <div>
+         <div>
+            <img src={logo} alt="" />
+            <p>Carry With care 2.0</p>
+         </div>
+         <div></div>
+      </div>
+
+   );
+};
+
+export default Navbar;
